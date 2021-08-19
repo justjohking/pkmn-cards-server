f213fc266d7c9625a00f5833a0d7ef8d26c51cb7
Houssame Zerouali <azenax328@gmail.com>
2021-08-19 14:31:25 +0200

    new card model

--- models/Collection.js
+++ models/Collection.js
@@ -6,9 +6,10 @@ const collectionSchema = new Schema({
   cards: [ {type: Schema.Types.ObjectId, ref: 'Card'} ],
   owner: {type: Schema.Types.ObjectId, ref: 'User'},
   type: [{type: String, enum:['Owned', 'Sell', 'Buy', 'Exchange']}],
-
 });
 
 const Collection = mongoose.model("Collection", collectionSchema);
 
 module.exports = Collection;
+
+
