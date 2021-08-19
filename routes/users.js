adf37ef714047914d54a3f01276666078a35f1b3
Houssame Zerouali <azenax328@gmail.com>
2021-08-19 15:36:03 +0200

    bid and some user routes

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,48 @@ const User = require("../models/User");
 const requireAuth = require("../middlewares/requireAuth");
 const router = express.Router();
 
-router.get("/me", requireAuth, (req, res, next) => {
+router.get("/user/profile", (req, res, next) => {
   User.findById(req.session.currentUser._id)
     .then((user) => {
       res.status(200).json(user);
     })
     .catch(next);
 });
+
+router.patch("/user/profile/edit", requireAuth, async (req, res, next) => {
+  const id = req.session.currentUser._id
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res
+      .status(400)
+      .json({ message: "id is not valid"})
+  }
+
+  const toUpdate = req.body;
+
+  try {
+    const updated = await User.findByIdAndUpdate(id, toUpdate, {
+      new: true,
+    });
+    
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err)
+  }
+
+});
+
+router.delete('/user/profile/delete', requireAuth, async (req, res, next) => {
+  const id = req.session.currentUser._id
+
+  try {
+    User.findByIdAndDelete(id)
+    return res.status(200)
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json(err)
+  }
+
+})
 
 module.exports = router;
