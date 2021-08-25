bc90326a41fcb657ded086a3bb668120cf46c82b
Johanna Angot <63612183+jooohke@users.noreply.github.com>
2021-08-25 09:06:38 +0200

    setting up api pokemon

--- routes/pokemonApi.js
+++ routes/pokemonApi.js
@@ -1,41 +1,59 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const pokemon = require('pokemontcgsdk')
+pokemon.configure({apiKey: process.env.API_KEY});
 
 router.get("/pokemonApi/all", async (req, res, next) => {
 
-    await axios
-    .get(`https://api.pokemontcg.io/v2/cards?api_key=${process.env.API_KEY}`)
-    .then((response) => {
-        res.status(200).json(response.data)
-    })
-    .catch(error => res.status(500).json(error))
-})
-
-router.get("/pokemonApi/all/:page", async (req, res, next) => {
-    // await axios
-    // .get(`https://api.pokemontcg.io/v2/cards?page=${req.params.page}&pageSize=40`)
-    // .then((response) => res.status(200).json(response.data))
-
-    await axios
-    .get(`https://api.pokemontcg.io/v2/cards?page=${req.params.page}&pageSize=40?api_key=${process.env.API_KEY}`)
-    .then((response) => res.status(200).json(response.data))
-})
-
-router.get("/pokemonApi/:id", async (req, res, next) => {
-    await axios
-    .get(`https://api.pokemontcg.io/v2/cards/${req.params.id}?api_key=${process.env.API_KEY}`)
-    .then((response) => {
-        res.status(200).json(response.data)
-    })
+    pokemon.card.all()
+    .then((cards) => { res.status(200).json(cards) })
     .catch(error => res.status(500).json(error))
 
     // await axios
-    // .get(`https://api.pokemontcg.io/v2/cards/${req.params.id}`)
+    // .get(`https://api.pokemontcg.io/v2/cards?api_key=${process.env.API_KEY}`)
     // .then((response) => {
     //     res.status(200).json(response.data)
     // })
     // .catch(error => res.status(500).json(error))
+})
+
+router.get("/pokemonApi/all/:page", async (req, res, next) => {
+
+    pokemon.card.where({ pageSize: 40, page: req.params.page})
+    .then(response => res.status(200).json(response))
+    .catch(error => res.status(500).json(error))
+
+    // await axios
+    // .get(`https://api.pokemontcg.io/v2/cards?page=${req.params.page}&pageSize=40`)
+    // .then((response) => res.status(200).json(response.data))
+
+    // await axios
+    // .get(`https://api.pokemontcg.io/v2/cards?page=${req.params.page}&pageSize=40?api_key=${process.env.API_KEY}`)
+    // .then((response) => res.status(200).json(response.data))
+})
+
+router.get("/pokemonApi/:id", async (req, res, next) => {
+
+    pokemon.card.find({q: "name:" + req.params.id})
+    .then(card => {res.status(200).json(card)})
+    .catch(error => console.log(error))
+
+    // await axios
+    // .get(`https://api.pokemontcg.io/v2/cards/${req.params.id}?api_key=${process.env.API_KEY}`)
+    // .then((response) => {
+    //     res.status(200).json(response.data)
+    // })
+    // .catch(error => res.status(500).json(error))
+})
+
+// Search by name
+router.get("/pokemonApi/search/:name", async (req, res, next) => {
+    // console.log(req.params.name)
+    await axios
+    .get(`https://api.pokemontcg.io/v2/cards?q=name:${req.params.name}`)
+    .then((response) => {res.status(200).json(response)})
+    .catch(error => res.status(500).json(error))
 })
 
 module.exports = router;
